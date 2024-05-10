03e1ebe832efe3d37cb3fbae6ae9d8030c494c0c
fatalistix <slavabal11@outlook.com>
2024-05-10 22:50:13 +0700

    added internal error

--- internal/database/errors.go
+++ internal/database/errors.go
@@ -10,3 +10,7 @@ var ErrCommandNotFound = errors.New("command not found")
 // process
 
 var ErrProcessNotFound = errors.New("process not found")
+
+// unknown
+
+var ErrInternal = errors.New("internal error")
