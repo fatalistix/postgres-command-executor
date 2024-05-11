92d84806bcc53250392d8ff9f4f1e51e1c4cbc14
fatalistix <slavabal11@outlook.com>
2024-05-11 21:14:46 +0700

    using env variables for connecting to db

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/fatalistix/postgres-command-executor/internal/database/postgres"
 	"github.com/fatalistix/postgres-command-executor/internal/database/postgres/repositories/command"
 	"github.com/fatalistix/postgres-command-executor/internal/database/postgres/repositories/process"
+	"github.com/fatalistix/postgres-command-executor/internal/env"
 	commandhandlers "github.com/fatalistix/postgres-command-executor/internal/http-server/handlers/command"
 	processhandlers "github.com/fatalistix/postgres-command-executor/internal/http-server/handlers/process"
 	"log/slog"
@@ -18,16 +19,16 @@ type App struct {
 	database *postgres.Database
 }
 
-func NewApp(log *slog.Logger, cfg config.Config) (*App, error) {
+func NewApp(log *slog.Logger, cfg config.Config, envVars *env.Env) (*App, error) {
 	const op = "app.NewApp"
 
 	database, err := postgres.NewDatabase(
-		cfg.Postgres.Host,
-		cfg.Postgres.Port,
-		cfg.Postgres.User,
-		cfg.Postgres.Password,
-		cfg.Postgres.DBName,
-		cfg.Postgres.SSLMode,
+		envVars.PostgresHost,
+		envVars.PostgresPort,
+		envVars.PostgresUser,
+		envVars.PostgresPassword,
+		envVars.PostgresDB,
+		envVars.PostgresSSLMode,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
