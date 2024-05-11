a19bb88a154f74a63e50c7c87e515ba93344e4b6
fatalistix <slavabal11@outlook.com>
2024-05-11 21:14:59 +0700

    removed Postgres struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,6 @@ import (
 
 type Config struct {
 	HTTPServer HTTPServer `json:"http_server"`
-	Postgres   Postgres   `json:"postgres"`
 }
 
 type HTTPServer struct {
@@ -16,15 +15,6 @@ type HTTPServer struct {
 	Timeout         time.Duration `json:"timeout"`
 	IdleTimeout     time.Duration `json:"idle_timeout"`
 	ShutdownTimeout time.Duration `json:"shutdown_timeout"`
-}
-
-type Postgres struct {
-	Host     string `json:"host"`
-	Port     uint16 `json:"port"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-	DBName   string `json:"dbname"`
-	SSLMode  string `json:"sslmode"`
 }
 
 func MustLoadConfig(configPath string) Config {
