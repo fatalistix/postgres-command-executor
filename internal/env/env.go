b0995bc7e256da475e7c0486922358bd85cdaa1c
fatalistix <slavabal11@outlook.com>
2024-05-11 21:15:13 +0700

    added struct with required env variables

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,10 +1,55 @@
 package env
 
-import "github.com/joho/godotenv"
+import (
+	"github.com/joho/godotenv"
+	"os"
+	"strconv"
+)
 
-func MustLoadEnv() {
+type Env struct {
+	ConfigPath       string
+	PostgresHost     string
+	PostgresPort     uint16
+	PostgresUser     string
+	PostgresPassword string
+	PostgresSSLMode  string
+	PostgresDB       string
+}
+
+func MustLoadEnv() *Env {
 	err := godotenv.Load()
 	if err != nil {
 		panic("cannot load environment variables: " + err.Error())
 	}
+
+	configPath := mustGetEnv("CONFIG_PATH")
+	postgresHost := mustGetEnv("POSTGRES_HOST")
+	postgresPort := mustGetEnv("POSTGRES_PORT")
+	postgresUser := mustGetEnv("POSTGRES_USER")
+	postgresPassword := mustGetEnv("POSTGRES_PASSWORD")
+	postgresSSLMode := mustGetEnv("POSTGRES_SSL_MODE")
+	postgresDb := mustGetEnv("POSTGRES_DB")
+
+	postgresPortUint, err := strconv.ParseUint(postgresPort, 10, 16)
+	if err != nil {
+		panic("cannot parse POSTGRES_PORT: " + err.Error())
+	}
+
+	return &Env{
+		ConfigPath:       configPath,
+		PostgresHost:     postgresHost,
+		PostgresPort:     uint16(postgresPortUint),
+		PostgresUser:     postgresUser,
+		PostgresPassword: postgresPassword,
+		PostgresSSLMode:  postgresSSLMode,
+		PostgresDB:       postgresDb,
+	}
+}
+
+func mustGetEnv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		panic(key + " is not set")
+	}
+	return value
 }
